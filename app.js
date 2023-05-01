ff1d2ebabe23642b81e69cb377e7679cc911162f
Amr Abdou <amrmanci11@yahoo.com>
2023-05-01 15:33:59 +0200

    using cookies and sessions

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
-require("dotenv").config();
+
+////////////////////////////////////////////////////////////using Cookies and sessions//////////////////////////
+
 const express= require("express");
 const bodyParser=require("body-parser");
 const ejs= require("ejs");
 const mongoose=require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds=10;
+const session= require("express-session");                               //1
+const passport= require("passport");                                     //2
+const passportLocalMongoose= require("passport-local-mongoose");         //3 no need to require passport-local, because it's included 
 
-const app =express();
+
+app =express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 
+
+app.use(session({                                                       //4  has to be exactly after all "use" and before connection
+    secret:"Our little secret.",
+    resave:false,
+    saveUninitialized:false,
+    expires: false    // set to false to make the cookie expire when the browsing session ends
+
+}));
+
+app.use(passport.initialize());                                        //5
+app.use(passport.session());                                           //6
 
                                             //DB
 
@@ -19,16 +34,22 @@ mongoose.set('strictQuery',false);                                        // to 
 mongoose.connect('mongodb://127.0.0.1/userDB');                 // the connection
 console.log("Connected to database");                                     //msg
 
-const userSchema = new mongoose.Schema({                              //Schema
+const userSchema = new mongoose.Schema({            //Schema          //7 Schema has to be like this not the standard shape 
     email:String,
     password:String
 });
 
+userSchema.plugin(passportLocalMongoose)                              //8 is used to hash and salt the password and to save users into mongodb
 
 
 
 
 const User=new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());                                  //9
+passport.serializeUser(User.serializeUser());                         //10
+passport.deserializeUser(User.deserializeUser());                     //11
+
 
 app.get("/", function(req,res){
     res.render("home");
@@ -42,43 +63,59 @@ app.get("/register", function(req,res){
     res.render("register");
 });
 
-app.post("/register", function(req, res){
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash){
 
-        const newUser=new User({
-            email:req.body.username,
-            password: hash           // using bcrypt hash for hashing with 10 salt Rounds 
-        });
-        newUser.save()
-        .then(() => {
-          res.render("secrets");
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-
-    });
-
-
-    
+app.get("/secrets", function(req,res){           // so simply this get request is as we have no route for secrets and we use it in post register
+    if(req.isAuthenticated()){                   // the user has to be able to access this page as long as he is logged in 
+        res.render("secrets");                   // if not he will be redirected to the login page
+    } else{
+        res.redirect("/login");
+    }
 });
 
-app.post("/login",function(req,res){
-    const username = req.body.username;
-    const password = req.body.password;        // using bcrypt.compare for comparing the hash, will compare hashed password with hashed hashed password in DB
-    User.findOne({email:username})
-    .then((foundUser) => {
-        if(foundUser){            
-                bcrypt.compare(password, foundUser.password, function(err, result){
-                    if(result===true){
-                        res.render("secrets");
-                    }
-                });
+app.get("/logout", function(req,res){            //logout Route that ends the session once using it
+    req.logout(function(err){                    // method logout comes from passport
+        if(err){
+            console.log(err);
+        }else{
+            res.redirect("/");
+        }
+    });    
+});
+
+
+
+
+app.post("/register", function(req, res){
+
+    User.register({username:req.body.username}, req.body.password)                                                        //Method register() is from passportLocalMongoose package
+      .then(function(user){
+        if(user){
+            passport.authenticate("local")(req,res, function(){
+                res.redirect("/secrets");
+            });
         }
       })
       .catch((err) => {
         console.log(err);
+        res.redirect("/register");
       });
+});
+
+app.post("/login",function(req,res){
+    const user=new User({
+        username:req.body.username,
+        password:req.body.password
+    });
+
+    req.login(user, function(err){                            //method login() comes from passport
+        if(err){
+            console.log(err);
+        } else{
+            passport.authenticate("local")(req,res, function(){
+                res.redirect("/secrets")
+            });
+        }
+    });                                           
 });
 
 
